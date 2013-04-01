9f240666eb2224046151077f0c7091fd5fc957e6
bydmm <521194743@qq.com>
2013-04-01 17:15:15 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bydmmblog::Application.config.secret_token = 'f6168bef63d7370015861807175ab79bb9d1db7bdd06a0a13ad835c53c51a5dcf4dae67b2a101c9917b501e013bd005752d788cd93d9ff52c25dc2a77bda8f31'
