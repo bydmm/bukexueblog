f817db066518469a375680784c5a960fda1bf13d
bydmm <521194743@qq.com>
2013-04-06 15:55:52 +0800

    sitemap

---
+++ sitemap.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'sitemap_generator'
+
+SitemapGenerator::Sitemap.default_host = 'http://www.bukexue.net'
+SitemapGenerator::Sitemap.create do
+  add '/blogs', :changefreq => 'daily', :priority => 0.9
+end
+SitemapGenerator::Sitemap.ping_search_engines # called for you when you use the rake task
